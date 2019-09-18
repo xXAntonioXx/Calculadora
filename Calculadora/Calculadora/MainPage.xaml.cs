81737a2cfd7dc14f1f9d3f445bad1e2faa2efa06
antonio gonzalez <jose_antony11@hotmail.com>
2019-09-18 00:28:16 -0700

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using org.mariuszgromada.math.mxparser;
+
+namespace Calculadora
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Calcular(Object sender,EventArgs e)
+        {
+            Expression eh = new Expression("(2*5)/6+2");
+            double resultado = eh.calculate();
+            DisplayAlert("Resultado", resultado.ToString(), "OK");
+        }
+    }
+
+}
